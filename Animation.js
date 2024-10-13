1b17c3586af9ef52d140b060ce9af46502d627e0
Abdul Haseeb Memon <142418450+Haseeb1717@users.noreply.github.com>
2024-10-13 17:04:56 +0400

    Create Animation.js

---
+++ Animation.js
@@ -0,0 +1,28 @@
+       ScrollReveal().reveal('#expense-title-inp', {
+            duration: 700,
+            origin: 'left',
+            distance: '100px',
+            delay: 600,
+        });
+
+        ScrollReveal().reveal('#expense-amount-inp', {
+            duration: 700,
+            origin: 'left',
+            distance: '100px',
+            delay: 800,
+        });
+
+        ScrollReveal().reveal('#expense-desc-inp', {
+            duration: 700,
+            origin: 'left',
+            distance: '100px',
+            delay: 1000,
+        });
+        
+        ScrollReveal().reveal('#addbtn', {
+            duration: 700,
+            origin: 'left',
+            distance: '100px',
+            delay: 1100,
+        });
+        
